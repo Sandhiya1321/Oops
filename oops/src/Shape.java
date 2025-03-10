7b995845fd45eebeb20821d5f8b24c802fdce54d
Sandhiya1321 <sandhiyasaprkle1234@gmail.com>
2025-03-10 19:36:43 +0530

    oops

---
+++ oops/src/Shape.java
@@ -0,0 +1,44 @@
+class Shape {
+    public double area() {
+        return 0.0; // Default implementation, can be overridden
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+}
+
+class ShapeArea {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+
+        System.out.println("Circle area: " + circle.area());
+        System.out.println("Rectangle area: " + rectangle.area());
+}
+}
+
