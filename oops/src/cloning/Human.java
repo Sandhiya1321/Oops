a3d1d2f52a4a2062f5a19059abbb016266a7854c
Sandhiya1321 <sandhiyasaprkle1234@gmail.com>
2025-04-14 23:00:53 +0530

    oops

--- oops/src/cloning/Human.java
+++ oops/src/cloning/Human.java
@@ -3,16 +3,20 @@ package cloning;
 public class Human implements Cloneable{
     int age;
     String name;
+    int[] arr;
 
     public Human(int age,String name){
         this.age=age;
         this.name=name;
+        this.arr=new int[]{3,6,7,8,9};
     }
     public Human(Human other){
         this.age=other.age;
         this.name=other.name;
     }
+    @Override
     public Object clone() throws CloneNotSupportedException{
+        //Shallow Copying
         return super.clone();
     }
 }
--- oops/src/cloning/Main.java
+++ oops/src/cloning/Main.java
@@ -1,10 +1,16 @@
 package cloning;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
         Human sandy = new Human(20, "sandy");
         //Human twin=new Human(sandy);
         Human twin = (Human) sandy.clone();
         System.out.println(twin.age+" "+twin.name);
+        System.out.println(Arrays.toString(twin.arr));
+        twin.arr[0]=100;
+        System.out.println(Arrays.toString(twin.arr));
+        System.out.println(Arrays.toString(sandy.arr));
     }
 }
